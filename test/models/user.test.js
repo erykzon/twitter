07312a5aa474aff32759a618cf8531bb9761b45a
Erykzon <jota.once6mail.com>
2022-04-21 20:53:26 -0500

    Add Requerimeinto 2 modificacion con su prueba

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,10 +1,17 @@
 const User = require('./../../app/models/User')
 
 describe ("Test para user", () =>{
-    test("Crear User" ,() => {
+    test("Requerimeinto 1 :Crear User" ,() => {
         const user = new User(1,"erykzon", "Erick","Bio","dateCreated","lasUpdate")
         expect(user.id).toBe(1)
         expect(user.username).toBe("erykzon")
     })
+    test("Requerimeinto 1 :Fechas en atributos de user", () => {
+
+        const user = new User (1,"")
+        expect(user.dateCreated).not.toBeUndefined()        
+        expect(user.lastUpdate).not.toBeUndefined()
+    })
+
 })
 
