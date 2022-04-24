61c40383d577b890e5339458be26c80305606854
Erykzon <jota.once6mail.com>
2022-04-24 17:13:52 -0500

    Agragando requerimiento usuuario valido con test

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -12,7 +12,24 @@ describe("Test for Userview", () =>{
     test("Return an error object when try to create a new user with a payload with invalid properties", () => {
         const playload = {username: null, name:12, id: "id"}
         const result = UserView.createUser(playload)
-        expect(result.error).toMatch(/necestas tener un valor valido/)
+        expect(result.error).toMatch(/necestan tener un valor valido/)
     })
+
+    test("Return an error object when try to create a new user with a playload with missin properties", () => {
+        const playload = {username: "Username"}
+        const result = UserView.createUser("playload")
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+
+    })
+
+    test("Create a user by a given valid playload", () => {
+        const playload = {username: "username", id: 1, name: "name"}
+        const result = UserView.createUser(playload)
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("username")
+        expect(result.id).toBe(1)
+
+    })
+    
 
 })
